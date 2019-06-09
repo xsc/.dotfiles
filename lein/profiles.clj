d88d6436bcb2c3a270aff7f120392de9cc17673d
Yannick Scherer <yannick.scherer@futurice.com>
2019-06-09 21:19:22 +0200

    Update lein-license plugin

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,6 +1,6 @@
 {:xsc {:plugins [[lein-ancient "0.6.14"]
                  [lein-try "0.4.3"]
-                 [lein-license "0.1.7-SNAPSHOT"]]
+                 [lein-license "0.1.8"]]
        :signing {:gpg-key "ED4FB884"}
        :aliases {"qi" ["ancient" "upgrade" ":no-tests" ":interactive"]
                  "q"  ["ancient" "upgrade" ":no-tests"]}}
